db66bc3eb4a4713a0415d4bf0ef680cd450b6cf6
adjish <adjish3@gmail.com>
2026-09-19 23:12:57 +0200

    Improve user interface

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if ! command -v cmake >/dev/null 2>&1; then
+  echo 'cmake: program not found!' >&2
+  exit 1
+fi
+
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 sudo cmake --install build
